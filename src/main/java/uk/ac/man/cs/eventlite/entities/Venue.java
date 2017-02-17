430a4dfaa8c5cdd2bf502f2ce3bac5dcb8e95010
TomMcKiernan <thomas.mckiernan-2@student.manchester.ac.uk>
2017-02-17 10:55:23 +0000

    Completed Part 1 for Persisiting Venues Methods for findAll, count and save added

--- src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
+++ src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
@@ -1,7 +1,15 @@
 package uk.ac.man.cs.eventlite.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "venues")
+
 public class Venue {
 
+	@Id
 	private long id;
 
 	private String name;
