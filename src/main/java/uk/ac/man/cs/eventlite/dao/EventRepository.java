5c3d75363202f404f46be12b5b0c1a3d76ea32a8
mbax4fc2 <fiona.campbell-8@student.manchester.ac.uk>
2017-02-17 12:16:27 +0000

    pushing EventRepository.java interface

---
+++ src/main/java/uk/ac/man/cs/eventlite/dao/EventRepository.java
@@ -0,0 +1,9 @@
+package uk.ac.man.cs.eventlite.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import uk.ac.man.cs.eventlite.entities.Event;
+
+public interface EventRepository extends CrudRepository<Event, Long> {
+	public Event findByName(String name);
+}
