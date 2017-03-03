64e76f77c6ee808ed2bce44404f9aca69858afd6
mfbyaww2 <mfbyaww2@E-C07KILF902.it.manchester.ac.uk>
2017-03-03 13:28:34 +0000

    Test whether detailed information(Id,Name,Venue,Date,Description)for single event are showed.

--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerWebTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerWebTest.java
@@ -1,6 +1,8 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import uk.ac.man.cs.eventlite.TestParent;
 
@@ -22,5 +25,35 @@ public class EventsControllerWebTest extends TestParent {
 	public void testGetAllEvents() throws Exception {
 		mvc.perform(get("/events").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
 				.andExpect(view().name("events/index"));
+	}
+	
+	@Test
+	public void getEventId() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/events/3?name=Third Event").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+		.andExpect(content().string(containsString("Id"))).andExpect(view().name("events/detail"));
+	}
+	
+	@Test
+	public void getEventDescription() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/events/3?name=Third Event").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+		.andExpect(content().string(containsString("Description"))).andExpect(view().name("events/detail"));
+	}
+	
+	@Test
+	public void getEventName() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/events/3?name=Third Event").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+		.andExpect(content().string(containsString("Third Event"))).andExpect(view().name("events/detail"));
+	}
+	
+	@Test
+	public void getEventVenue() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/events/3?name=Third Event").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+		.andExpect(content().string(containsString("Alan Gilbert"))).andExpect(view().name("events/detail"));
+	}
+	
+	@Test
+	public void getEventDate() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/events/3?name=Third Event").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+		.andExpect(content().string(containsString("2013-01-09"))).andExpect(view().name("events/detail"));
 	}
 }
