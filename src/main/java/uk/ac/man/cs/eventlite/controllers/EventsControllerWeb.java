0753a6e9e1b02d0ff63fe38ea2bbdf4f074072d9
TomMcKiernan <thomas.mckiernan-2@student.manchester.ac.uk>
2017-03-07 13:53:01 +0000

    Initial commit for add event branch Currently adds new event, with id, name and date. No tests

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerWeb.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerWeb.java
@@ -1,14 +1,24 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import java.util.Date;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 
 @Controller
 @RequestMapping("/events")
@@ -28,5 +38,26 @@ public class EventsControllerWeb {
 
 		return "events/index";
 	}
+	
+	@RequestMapping(value = "/new", method = RequestMethod.GET, produces = { MediaType.TEXT_HTML_VALUE })
+	public String newGreetingHtml(Model model) {
+		return "events/new";
+	}
 
+
+	@RequestMapping(value = "/{eventname}/{eventdate}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = {
+			MediaType.TEXT_HTML_VALUE })
+	public String createEventFromForm(@PathVariable("eventname") String eventname, @PathVariable("eventdate") String date,
+			@RequestParam(value = "eventname", defaultValue = "Empty") String name, 
+			@RequestParam(value = "eventdate", defaultValue = "Empty") Date edate,
+			Model model) {
+
+		Event event = new Event();
+		event.setName(name);
+		event.setDate(edate);
+		
+		eventService.save(event);
+
+		return "redirect:/events/";
+	}
 }
