284a34222b369543d7621d67f0db6e152fd56317
Joe Powell <joseph.powell-2@student.manchester.ac.uk>
2017-05-07 13:02:28 +0100

    Added simple tests and REST functionality for venues

---
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerRestTest.java
@@ -0,0 +1,58 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import uk.ac.man.cs.eventlite.TestParent;
+import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
+
+@AutoConfigureMockMvc
+public class VenuesControllerRestTest extends TestParent {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private EventService eventService;
+	
+	@Autowired
+	private VenueService venueService;
+
+	@Test
+	public void testGetAllVenues() throws Exception {
+
+		mvc.perform(get("/venues").accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.title", equalTo("EventLite Venues")))
+			.andExpect(jsonPath("$._self", equalTo("http://localhost/venues")));
+		
+	}
+	
+	@Test
+	public void testSearchAVenue() throws Exception {
+		
+		Venue venue1 = new Venue();
+ 		venue1.setName("Kilburn");
+ 		venue1.setCapacity(1000);
+ 		venueService.save(venue1);
+		
+		mvc.perform(get("/venues/search?searchVenue=Kilburn").accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.title", equalTo("EventLite Venues")))
+			.andExpect(jsonPath("$._self", equalTo("http://localhost/venues/search")))
+			.andExpect(jsonPath("$.venues[0].name", equalTo(venue1.getName())));
+		
+	}
+}
