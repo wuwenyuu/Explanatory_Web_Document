848f650ad90c71d6c9577fab0d154c00a58c4a28
Joe Powell <joseph.powell-2@student.manchester.ac.uk>
2017-05-07 15:10:14 +0100

    Fixed referencing in the inital data loader

--- src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
@@ -53,21 +53,18 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
 		Date time3 = Time.valueOf("15:00:00");
 		
 		Venue venue1 = new Venue();
- 		venue1.setId(4);
  		venue1.setName("Kilburn");
 		venue1.setAddress("Kilburn Building, Oxford Road, Manchester, England, M13 9PL");
  		venue1.setCapacity(1000);
  		venueService.save(venue1);
  		
  		Venue venue2 = new Venue();
- 		venue2.setId(5);
  		venue2.setName("Stopford");
 		venue2.setAddress("Stopford Building, Oxford Road, Manchester, England, M13 9PG");
  		venue2.setCapacity(500);
  		venueService.save(venue2);
 
  		Venue venue3 = new Venue();
- 		venue3.setId(6);
  		venue3.setName("Alan Gilbert");
 		venue3.setAddress("Alan Gilbert Learning Commons, Oxford Road, Manchester, England, M13 9PP");
  		venue3.setCapacity(750);
@@ -75,31 +72,25 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
  		
 		// Build and save initial models here.
 		Event event1 = new Event();
-		event1.setId(4);
 		event1.setDate(date1);
 		event1.setTime(time1);
 		event1.setName("Concert Event");
-		venue1=venueService.findById(4);
 		event1.setVenue(venue1);
 		event1.setDescription("This is Concert");
 		eventService.save(event1);
 		
 		Event event2 = new Event();
-		event2.setId(5);
 		event2.setDate(date2);
 		event2.setTime(time2);
 		event2.setName("Gnother Event");
-		venue2=venueService.findById(5);
 		event2.setVenue(venue2);
 		event2.setDescription("This is another event");
 		eventService.save(event2);
 		
 		Event event3 = new Event();
-		event3.setId(6);
 		event3.setDate(date3);
 		event3.setTime(time3);
 		event3.setName("Third Event");
-		venue3=venueService.findById(6);
 		event3.setVenue(venue3);
 		event3.setDescription("This is third event");
 		eventService.save(event3);	
